705ec81b478d785949fbd9399d64a82c5f24c2f2
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-14 13:19:13 -0300

    ENH: improve base fetching

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,15 +1,28 @@
+import NewTasksListDialog from "@/components/NewTasksListDialog";
 import TasksListCard from "@/components/TasksListCard";
+import { apiUrl } from "@/lib/utils";
+import { TasksList } from "@/types/entities";
 import React from "react";
 
-export default function UserPage() {
+export default async function UserPage() {
+  // TODO: use var as userId
+  const tasksListsData = await fetch(
+    `${apiUrl}/tasks-lists?userId=cluzkodz20000vg1886jcykru`,
+    {
+      method: "GET",
+    },
+  );
+
+  const tasksLists: TasksList[] = await tasksListsData.json();
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 pt-[100px]">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 px-4 pt-[100px]">
       <h1 className="text-[2rem] font-bold">My To-Dos Lists</h1>
-      <div className="flex flex-col gap-4">
-        <TasksListCard />
-        <TasksListCard />
-        <TasksListCard />
-        <TasksListCard />
+      <NewTasksListDialog />
+      <div className="flex w-full flex-col items-center gap-4">
+        {tasksLists.map((tasksList) => (
+          <TasksListCard key={tasksList.id} tasksList={tasksList} />
+        ))}
       </div>
     </div>
   );
