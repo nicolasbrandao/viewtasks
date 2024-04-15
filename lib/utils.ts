4d63119f901e3294846e48db1a865d271848501f
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-15 10:48:16 -0300

    FIX: fixed var name convention

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
