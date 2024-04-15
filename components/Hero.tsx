662b9b17cbf0869b0c2ceead31830e3848b6e264
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-15 11:06:49 -0300

    FEA: change hero icon and remove unused asset

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
-import { ListChecks } from "lucide-react";
+import { MonitorCheck } from "lucide-react";
 import React from "react";
 
 export default function Hero() {
   return (
     <>
-      <ListChecks className="h-[180px] w-[180px]" />
+      <MonitorCheck className="h-[250px] w-[250px]" />
       <div className="mx-auto flex w-full max-w-[400px] flex-col items-center justify-center px-4 text-center font-bold">
         <h1 className="text-[3rem]">Just To-Do It</h1>
         <h2 className="text-[2rem]">Your #1 platform to just do it!</h2>
