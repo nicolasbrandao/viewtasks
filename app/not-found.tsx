cf95409d10780f58ccafc8deef0a03df55d06617
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-15 18:03:06 -0300

    ENH: minor not found page design improvement

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bold text-xl">Not Found</h1>
+      <p>Could not find requested page</p>
+      <Link
+        className="w-fit px-4 text-muted-foreground underline"
+        href={"/"}
+      >
+        Return Home
+      </Link>
+    </main>
   );
 }
