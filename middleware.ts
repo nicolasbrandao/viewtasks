ce82d335234227526f1fd98c8a7308c2948dcc7d
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-16 11:27:57 -0300

    FIX: use absolute path

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(req: NextRequest) {
     return NextResponse.next();
 
   const accessToken = req.cookies.get("access_token");
-  if (!accessToken) return NextResponse.redirect("/login");
+  if (!accessToken) return NextResponse.redirect(`${new URL(req.url).origin}/login`);
 
   const headers = new Headers(req.headers);
   headers.set("Authorization", `Bearer ${accessToken.value}`);
